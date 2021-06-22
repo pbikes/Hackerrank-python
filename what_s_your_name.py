4464cca412a91c941c71950bf1ac179a1fa3ffa4
pbikes <84617291+pbikes@users.noreply.github.com>
2021-06-22 17:18:24 +0200

    what's your name task two possible solutions

---
+++ what_s_your_name.py
@@ -0,0 +1,6 @@
+def print_full_name(first, last):
+    print("Hello " + first + " " + last + "! You just delved into python.")
+    # print("Hello {0} {1} ! You just delved into python.".format(first, last))
+
+
+print_full_name("Ross", "Taylor")
