a94d229af66915637f06c7b6aeb245a5a6b73693
pbikes <84617291+pbikes@users.noreply.github.com>
2021-06-28 20:49:23 +0200

    string validator second solution with one for cycle

--- string_validators.py
+++ string_validators.py
@@ -1,6 +1,27 @@
 if __name__ == '__main__':
     s = input()
-    if any(char.isalnum() for char in s):
+    isalnum = False
+    isalpha = False
+    isdigit = False
+    islower = False
+    isupper = False
+    for i in s:
+        if i.isalnum():
+            isalnum = True
+        if i.isalpha():
+            isalpha = True
+        if i.isdigit():
+            isdigit = True
+        if i.islower():
+            islower = True
+        if i.isupper():
+            isupper = True
+    print(isalnum)
+    print(isalpha)
+    print(isdigit)
+    print(islower)
+    print(isupper)
+    """if any(char.isalnum() for char in s):
         print(True)
     else:
         print(False)
@@ -19,4 +40,4 @@ if __name__ == '__main__':
     if any(char.isupper() for char in s):
         print(True)
     else:
-        print(False)
+        print(False)"""
