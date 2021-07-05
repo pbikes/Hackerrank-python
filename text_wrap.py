c88f0c05fffc54ecd1a5efcf2a2278ae4e1ac0e0
pbikes <84617291+pbikes@users.noreply.github.com>
2021-07-05 22:34:11 +0200

    text_wrap add one line solution by commit

--- text_wrap.py
+++ text_wrap.py
@@ -8,3 +8,4 @@ if __name__ == '__main__':
     string, max_width = input(), int(input())
     result = wrap(string, max_width)
     print(result)
+#print("\n".join([string[i:i + max_width] for i in range(0, len(string), max_width)]))
